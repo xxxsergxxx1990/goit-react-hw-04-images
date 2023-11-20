450874953a1dfd228f8958a439f52b8404c28f49
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-11-20 16:33:33 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,35 @@
-import { Component } from 'react';
+import React, { useEffect,useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import s from '../Modal/Modal.module.css';
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = ({ onModalClose, largeImageURL }) => {
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
 
-  handleKeyDown = e => {
-    if (e.keyCode === 27 || e.currentTarget === e.target) {
-      return this.props.onModalClose();
-    }
-  };
 
-  render() {
-    const { largeImageURL } = this.props;
-    return (
-      <div className={s.Overlay} onClick={this.handleKeyDown}>
-        <div className={s.Modal}>
-          <img src={largeImageURL} alt="" />
-        </div>
+  const keyDown = useCallback(
+    (e) => {
+      if (e.keyCode === 27 || e.currentTarget === e.target) {
+        onModalClose();
+      }
+    },
+    [onModalClose]
+  );
+
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDown);
+    return window.removeEventListener('keydown', keyDown);
+  }, [keyDown]);
+
+  return (
+    <div className={s.Overlay} onClick={keyDown}>
+      <div className={s.Modal}>
+        <img src={largeImageURL} alt="" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   onModalClose: PropTypes.func,
